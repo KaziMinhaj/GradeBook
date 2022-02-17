f162c3d588a1b44bd7571e350c80b480814ec95d
Kazi Minhaj <kazi.minhaj@zerod.ai>
2022-02-17 21:48:53 +0600

    fresh commit

---
+++ Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public Book(string name) 
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        private  List<double> grades;
+        private string name;
+        public void AddGrade(double grade) {
+            grades.Add(grade);
+        }
+        public void ShowStatictics() {
+            var result = 0.0;
+            var highgrade = double.MinValue;
+            var lowgrade = double.MaxValue;
+            foreach (var number in grades) { 
+                lowgrade = Math.Min(number,lowgrade);
+                highgrade = Math.Max(number,highgrade);
+                result = result + number;
+            }
+
+            result = result / grades.Count;
+        
+            Console.WriteLine($"The lowest grade is {lowgrade}");
+            Console.WriteLine($"The highest grade is {highgrade}");
+            Console.WriteLine($"Avarage is {result:N1}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    public class Program
+    {
+        static void Main(String[] args) {
+            var book = new Book("Scott grade book");
+            book.AddGrade(90);
+            book.AddGrade(85);
+            
+            book.ShowStatictics();
+        }
+    }
+}
