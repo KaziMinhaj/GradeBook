91041c9d73d4448cc76878782a9827d7a3fc3820
Kazi Minhaj <kazi.minhaj@zerod.ai>
2022-02-20 17:49:10 +0600

    module 7

--- Program.cs
+++ Program.cs
@@ -10,14 +10,39 @@ namespace GradeBook
     {
         static void Main(String[] args) {
             var book = new Book("Scott grade book");
-            book.AddGrade(90);
-            book.AddGrade(85);            
+            var done = false;
+            while(!done){ 
+                Console.WriteLine("Enter a grade or enter q to quit.");
+                var input = Console.ReadLine();
+                if (input == "q") 
+                {
+                    done = true;
+                    continue;
+                }
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade); 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+               
+            }
+
+
+
+
+
+
             var stats = book.GetStatictics();
 
             //display
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"Avarage is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
